ee2f6c3e78e1c01e5b133a736e529553c5ab4ff8
Noamane CHIRAOUEH <noamane.chiraoueh@gmail.com>
2020-04-22 15:37:02 +0000

    Basic add todo items

--- src/App.js
+++ src/App.js
@@ -2,17 +2,51 @@ import React, {Component} from 'react';
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import uuid from "uuid";
+import {v4 as uuid} from "uuid";
 
 class App extends Component{
+
+    state = {
+        items: [],
+        id: uuid(),
+        item:'',
+        editItem: false
+    }
+
+    handleChange = (e)=>{
+        this.setState({item:e.target.value})
+    }
+
+    handleSubmit = (e)=>{
+    e.preventDefault();
+
+    const newItem = {
+        id: this.state.id,
+        title: this.state.item
+    }
+
+    console.log(newItem);
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+        items: updatedItems,
+        item: '',
+        id:uuid(),
+        editItem: false
+    })
+    }
     render(){
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-10 mx-auto col-md-8 mt-4">
                         <h3 className="text-capitalize text-center">todo input</h3>
-                        <TodoInput />
-                        <TodoList />
+                        <TodoInput item={this.state.item}
+                                   handleChange={this.handleChange}
+                                   handleSubmit={this.handleSubmit}
+                        />
+                        <TodoList items={this.state.items} />
                     </div>
                 </div>
             </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,23 @@ import React, {Component} from 'react';
 import TodoItem from "./TodoItem";
 
 class TodoList extends Component{
+
     render(){
+        const {items} = this.props
+
         return (
             <ul className="list-group my-5">
                 <h3 className="text-capitalize text-center">todo list</h3>
-                <TodoItem/>
+                {
+                    items.map(item => {
+                        return (
+                            <TodoItem key={item.id} title={item.title} />
+                        );
+                    })
+                }
+
+
+
                 <button type="submit" className="btn btn-danger btn-block text-capitalize mt-5">
                     clear list
                 </button>
